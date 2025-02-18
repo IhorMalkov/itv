da98b11f855f59a7d0d6e4ddf5ffdf42f3450804
unknown <igorzno2004@gmail.com>
2025-02-18 14:01:36 -0800

    Refactored the app logic, made api work faster

--- itv/app/api/scrape/route.ts
+++ itv/app/api/scrape/route.ts
@@ -1,41 +1,22 @@
 import { NextResponse } from "next/server";
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
-import NodeCache from "node-cache";
-import { supabase } from "@/lib/supabase";
+import { insertTeamsData } from "@/lib/database";
 
 puppeteer.use(StealthPlugin());
 
-const cache = new NodeCache({ stdTTL: 300, checkperiod: 60 });
 
 export async function GET() {
   try {
-    const cachedData = cache.get("teamData");
-    if (cachedData) {
-      console.log("Serving data from cache");
-      return NextResponse.json(cachedData);
-    }
-
     console.log("Scraping new data...");
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
-    const url = "https://www.hltv.org/ranking/teams/2024/december/16";
+    const url = "https://www.hltv.org/ranking/teams/2025/february/10";
 
     await page.setUserAgent(
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
     );
     await page.goto(url, { waitUntil: "networkidle2" });
-
-    try {
-      const [acceptButton] = await page.$x(
-        "//button[text()='Allow all Cookies']"
-      );
-      if (acceptButton) {
-        await acceptButton.click();
-      }
-    } catch (error) {
-      console.log("No cookie banner found or already accepted.");
-    }
 
     const positions = await page.$$eval(".position", (elements) =>
       elements.slice(0, 20).map((el) => el.textContent?.trim())
@@ -62,20 +43,7 @@ export async function GET() {
 
     await browser.close();
 
-    // Store the scraped data in Supabase
-    const { error } = await supabase
-      .from("team")
-      .upsert(teamsData, { onConflict: ["name"] });
-    if (error) {
-      console.error("Error inserting data into Supabase:", error);
-      return NextResponse.json(
-        { error: "Failed to insert data into Supabase." },
-        { status: 500 }
-      );
-    }
-
-    cache.set("teamData", teamsData);
-    console.log("Data cached successfully");
+    await insertTeamsData(teamsData);
 
     return NextResponse.json(teamsData);
   } catch (error) {
---
+++ itv/lib/database.ts
@@ -0,0 +1,12 @@
+import {supabase} from "./supabase";
+
+export async function insertTeamsData(teamsData) {
+    const { error } = await supabase
+        .from("team")
+        .upsert(teamsData, { onConflict: ["name"] });
+
+    if (error) {
+        console.error("Error inserting data into Supabase:", error);
+        throw new Error("Failed to insert data into Supabase.");
+    }
+}
